8ade229a101251caeb3daf596f97e665cb474e8a
Acampora <92322511+AcamporaV2@users.noreply.github.com>
2024-06-10 12:56:04 +0200

    Create esempio3.c

---
+++ Scheudling FOR/esempio3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+#include <unistd.h>
+
+//Ad ogni iterazione i thread aspettano un numero di secondi pari a l’indice dell’iterazione.
+
+int main( )
+{
+  
+  int n_threads, i;
+
+    #pragma omp parallel for private(i) schedule(static) num_threads(4)
+    for (i=0; i<16; i++)
+    {
+      //aspetta un numero pari ad i secondi
+      sleep(i);
+      printf(“Il thread %d ha completato iterazione %d.\n", omp_get_thread_num( ) , i);
+    }
+printf(“Tutti I thread hanno terminato!\n");
+return 0;
+}
+
+//Se testato si nota che lo scheduling non è ben organizzato
